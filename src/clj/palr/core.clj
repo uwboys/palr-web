88ff503040c1bc639d04effe0837341a440c266b
Ramanpreet Nara <ramanpreet.nara@gmail.com>
2016-10-10 20:53:55 -0400

    feat(clj/palr/core): Implement mock authentication

--- src/clj/palr/core.clj
+++ src/clj/palr/core.clj
@@ -1,12 +1,25 @@
 (ns palr.core
   (:require [compojure.core :refer :all]
+            [ring.util.response :refer [response status]]
             [ring.middleware.reload :refer [wrap-reload]]
             [ring.middleware.json :refer [wrap-json-body wrap-json-response]]
             [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
-            [clojure.pprint :refer [pprint]])) ; httpkit is a server
+            [clojure.pprint :refer [pprint]]))
+
+(def users #{["ramanpreet" "isawesome"]})
+(def tokens {["ramanpreet" "isawesome"] "Ramanpreet's special token!"})
 
 (defroutes api
-  (POST "/login" {body :body} (do (pprint body) "what... horses?")))
+  (POST "/login" {{:keys [username password]} :body}
+    (let [creds [username password]]
+      (pprint creds)
+      (if (contains? users creds)
+        (-> {:access-token (tokens creds)}
+            (response)
+            (status 200))
+        (-> {:error "Invalid credentials."}
+            (response)
+            (status 422))))))
 
 (def app
   (-> api
